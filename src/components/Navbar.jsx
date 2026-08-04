3a3d4edab316cd150ca509e2c25d5e7d6842e4b4
Sandhiya Shree . S <sandhiyashreesandhiya2006@gmail.com>
2026-08-04 10:11:58 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
-  { name: "Internship", href: "#experience" }, // or "#internship" if that's your id
+  { name: "Experience", href: "#experience" }, 
   { name: "Education", href: "#education" },
   { name: "Certifications", href: "#certifications" },
   { name: "Contact", href: "#contact" },
